c0d81a042f6e442bcf97c4896a02357877829e37
Kesmek <justin.scopelleti@gmail.com>
2021-08-07 00:05:31 -0400

    flatlist -> sectionList

--- src/containers/Punches/index.tsx
+++ src/containers/Punches/index.tsx
@@ -1,6 +1,28 @@
-import { useLayoutEffect } from "react";
-import { Text, View, ListRenderItem, Alert } from "react-native";
-import { BaseButton, FlatList, RectButton } from "react-native-gesture-handler";
+import { useEffect, useLayoutEffect, useState } from "react";
+import {
+  Text,
+  View,
+  Alert,
+  SectionList,
+  SectionListData,
+  SectionListRenderItem,
+} from "react-native";
+import {
+  BaseButton,
+  BorderlessButton,
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+  RectButton,
+} from "react-native-gesture-handler";
+import Animated, {
+  AnimatedLayout,
+  FadeInUp,
+  FadeOutUp,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import { useAppDispatch, useAppSelector } from "../../redux";
 import {
@@ -9,15 +31,47 @@ import {
   PunchRecord,
   punchIn,
   punchOut,
+  createSelectSectionedPunches,
 } from "../../redux/punches";
 import { PunchesNavigationProps } from "../../types/navigation";
 import { colors } from "../../utils/constants";
 import { calculateHours, formatDate } from "../../utils/functions";
 import styles from "./styles";
 
+const AnimatedBaseButton = Animated.createAnimatedComponent(BaseButton);
+
 const Punches = ({ navigation }: PunchesNavigationProps) => {
   const data = useAppSelector(createSelectPunches());
+  const sectionedData = useAppSelector(createSelectSectionedPunches());
   const dispatch = useAppDispatch();
+
+  const [showMore, setShowMore] = useState(false);
+
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const animatedGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    { offsetX: number; offsetY: number }
+  >({
+    onActive: (event) => {
+      translateX.value = event.translationX;
+      translateY.value = event.translationY;
+    },
+    onEnd: () => {
+      translateX.value = withSpring(0);
+      translateY.value = withSpring(0);
+    },
+  });
+
+  const animatedButton = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value / 2 },
+        { translateY: translateY.value / 2 },
+      ],
+    };
+  });
 
   const createDay = () => {
     const today = new Date();
@@ -41,57 +95,95 @@ const Punches = ({ navigation }: PunchesNavigationProps) => {
     }
   };
 
-  const renderItem: ListRenderItem<PunchRecord> = ({ item, index }) => {
+  const handlePunchOut = (inDate: Date, outDate: Date, index: number) => {
+    if (inDate.getTime() === 0) {
+      Alert.alert(
+        "Punch-In Missing",
+        "You must punch in before you can punch out.",
+        [
+          {
+            text: "ok",
+          },
+        ],
+      );
+    } else if (outDate.getTime() === 0) {
+      dispatch(punchOut(index));
+    }
+  };
+
+  const handleCalculateHours = () => {
+    setShowMore(false);
+  };
+
+  const renderSectionHeader = ({
+    section: { month, data },
+  }: {
+    section: SectionListData<PunchRecord>;
+  }) => {
+    // if (data.length) {
+    return (
+      <Text
+        style={{
+          backgroundColor: colors.BACKGROUND,
+          color: colors.PRIMARY_WHITE,
+          fontSize: 24,
+          fontWeight: "bold",
+          paddingLeft: 15,
+        }}
+      >
+        {month}
+      </Text>
+    );
+    // } else {
+    //   return null;
+    // }
+  };
+
+  const renderItem: SectionListRenderItem<PunchRecord> = ({ item, index }) => {
     const rawDate = new Date(item.date);
     const rawPunchIn = new Date(item.punchIn ?? 0);
     const rawPunchOut = new Date(item.punchOut ?? 0);
-    const clockIn = formatDate(rawPunchIn);
-    const clockOut = formatDate(rawPunchOut);
     const date = formatDate(rawDate);
 
     return (
       <RectButton
-        style={styles.itemContainer}
         onPress={() => {
           navigation.navigate("Edit Punch", { index });
         }}
+        style={styles.itemContainer}
       >
         <View style={styles.dateContainer}>
           <Text
             style={styles.date}
-          >{`${date.day}${date.suffix} ${date.month}`}</Text>
+          >{`${date.month} ${date.day}${date.suffix}`}</Text>
           <Text style={styles.dayOfWeek}>{date.dayOfWeek}</Text>
         </View>
         <View style={styles.punchContainer}>
           <RectButton
-            style={styles.punchItem}
             onPress={() => {
               if (rawPunchIn.getTime() === 0) {
                 dispatch(punchIn(index));
               }
             }}
+            style={styles.punchItem}
           >
             <Icon
               name={"login"}
               size={32}
               style={{ color: colors.SECONDARY_GREEN }}
             />
-            <Text style={styles.time}>{clockIn.time}</Text>
+            <Text style={styles.time}>{formatDate(rawPunchIn).time}</Text>
           </RectButton>
           <RectButton
+            onPress={() => handlePunchOut(rawPunchIn, rawPunchOut, index)}
             style={styles.punchItem}
-            onPress={() => {
-              if (rawPunchOut.getTime() === 0) {
-                dispatch(punchOut(index));
-              }
-            }}
           >
             <Icon
               name={"logout"}
               size={32}
               style={{ color: colors.SECONDARY_RED }}
             />
-            <Text style={styles.time}>{clockOut.time}</Text>
+            <Text style={styles.time}>{formatDate(rawPunchOut).time}</Text>
           </RectButton>
           <View style={styles.punchItem}>
             <Icon
@@ -111,33 +203,56 @@ const Punches = ({ navigation }: PunchesNavigationProps) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       // eslint-disable-next-line react/no-unstable-nested-components
-      headerRight: () => (
-        <BaseButton
-          onPress={() => console.log("more")}
-          style={{ borderRadius: 20 }}
+      headerRight: ({ pressColor, pressOpacity }) => (
+        <BorderlessButton
+          activeOpacity={pressOpacity}
+          onPress={() => setShowMore(!showMore)}
+          rippleColor={pressColor}
         >
-          <Icon name={"more-vert"} color={colors.PRIMARY_WHITE} size={28} />
-        </BaseButton>
+          <Icon color={colors.PRIMARY_WHITE} name={"more-vert"} size={28} />
+        </BorderlessButton>
       ),
     });
-  }, [navigation]);
+  }, [navigation, showMore]);
 
   return (
     <View style={styles.root}>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.date + item.punchIn}
-        ItemSeparatorComponent={() => <View style={{ height: 2 }} />}
-      />
-      <BaseButton
-        style={styles.newDateButton}
-        onPress={createDay}
-        // entering={ZoomIn.springify()}
-        // exiting={ZoomOut}
-      >
-        <Icon name={"add"} color={colors.PRIMARY_WHITE} size={50} />
-      </BaseButton>
+      <AnimatedLayout style={{ flex: 1 }}>
+        <SectionList
+          ItemSeparatorComponent={() => <View style={{ height: 2 }} />}
+          keyExtractor={(item, index) => `${item.date}${index}`}
+          renderItem={renderItem}
+          renderSectionHeader={renderSectionHeader}
+          sections={sectionedData}
+          SectionSeparatorComponent={() => <View style={{ height: 2 }} />}
+        />
+        {showMore && (
+          <AnimatedBaseButton
+            entering={FadeInUp.duration(150)}
+            exiting={FadeOutUp.duration(150)}
+            onPress={() => setShowMore(false)}
+            rippleColor={"transparent"}
+            style={{
+              height: "100%",
+              position: "absolute",
+              width: "100%",
+            }}
+          >
+            <Animated.View style={styles.moreContainer}>
+              <RectButton onPress={handleCalculateHours}>
+                <Text style={styles.moreText}>Calculate Hours</Text>
+              </RectButton>
+            </Animated.View>
+          </AnimatedBaseButton>
+        )}
+        <PanGestureHandler onGestureEvent={animatedGestureEvent}>
+          <Animated.View style={[animatedButton, styles.newDateWrapper]}>
+            <BaseButton onPress={createDay} style={styles.newDateButton}>
+              <Icon color={colors.PRIMARY_WHITE} name={"add"} size={50} />
+            </BaseButton>
+          </Animated.View>
+        </PanGestureHandler>
+      </AnimatedLayout>
     </View>
   );
 };
--- src/containers/Punches/styles.ts
+++ src/containers/Punches/styles.ts
@@ -2,50 +2,71 @@ import { StyleSheet } from "react-native";
 import { colors } from "../../utils/constants";
 
 export default StyleSheet.create({
-  root: {
+  date: { fontSize: 20, fontWeight: "bold" },
+  dateContainer: {
     flex: 1,
   },
+  dayOfWeek: { fontSize: 18, marginLeft: 2 },
+  editButton: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemContainer: {
+    alignItems: "center",
+    backgroundColor: colors.BORDER,
+    flexDirection: "row",
+    height: 65,
+    justifyContent: "space-between",
+    paddingLeft: 15,
+  },
+  moreContainer: {
+    backgroundColor: colors.BACKGROUND,
+    borderColor: "#222222",
+    borderWidth: 2,
+    position: "absolute",
+    right: 0,
+    width: 200,
+  },
+  moreText: {
+    color: colors.PRIMARY_WHITE,
+    fontSize: 18,
+    padding: 8,
+    textAlign: "left",
+  },
+  newDateButton: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    width: "100%",
+  },
+  newDateWrapper: {
+    alignItems: "center",
+    backgroundColor: colors.PRIMARY_PURPLE,
+    borderRadius: 75 / 2,
+    bottom: 50,
+    height: 75,
+    justifyContent: "center",
+    overflow: "hidden",
+    position: "absolute",
+    right: 50,
+    width: 75,
+  },
   punchContainer: {
-    flex: 2,
+    flex: 1.75,
     flexDirection: "row",
     justifyContent: "space-evenly",
   },
   punchItem: {
+    alignItems: "center",
+    justifyContent: "center",
     width: 50,
-    justifyContent: "center",
-    alignItems: "center",
   },
-  editButton: {
-    justifyContent: "center",
-    alignItems: "center",
+  root: {
+    flex: 1,
   },
   time: {
     color: colors.BLACK,
     fontSize: 16,
     fontWeight: "bold",
-  },
-  date: { fontSize: 20, fontWeight: "bold" },
-  dayOfWeek: { fontSize: 18, marginLeft: 2 },
-  itemContainer: {
-    backgroundColor: colors.BORDER,
-    height: 65,
-    alignItems: "center",
-    paddingLeft: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  dateContainer: {
-    flex: 1,
-  },
-  newDateButton: {
-    width: 75,
-    height: 75,
-    backgroundColor: colors.PRIMARY_PURPLE,
-    position: "absolute",
-    bottom: 50,
-    right: 50,
-    borderRadius: 75 / 2,
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
