86c44b1f09e6b89b51c7e903e71a593a64bb9b5e
Kesmek <justin.scopelleti@gmail.com>
2022-01-11 17:40:22 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+  rules: {
+    'react/jsx-no-useless-fragment': 1,
+    'react-native/no-unused-styles': 1,
+    'prettier/prettier': 0,
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
