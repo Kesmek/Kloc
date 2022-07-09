e47d94fac3e014eb30cbba16b4922677361b1674
Justin <justin.scopelleti@gmail.com>
2022-07-09 13:22:25 -0400

    chore: Upgrade to React Native 0.69.1

--- android/app/src/main/java/com/puncher/MainApplication.java
+++ android/app/src/main/java/com/puncher/MainApplication.java
@@ -13,9 +13,6 @@ import com.puncher.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -37,11 +34,6 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
-        }
-
-        @Override
-        protected JSIModulePackage getJSIModulePackage() {
-            return new ReanimatedJSIModulePackage();
         }
       };
 
