558a07ee996d001ac15a848e054134a971d84c9e
justin <justin.scopelleti@gmail.com>
2025-05-03 18:46:03 -0400

    fix: button right in jobs header

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,6 +1,26 @@
-import { Stack } from "expo-router";
+import Icon from "@/components/Icon";
+import { Link, Stack, useFocusEffect } from "expo-router";
+import { BorderlessButton } from "react-native-gesture-handler";
+import type { NativeStackHeaderRightProps } from "@react-navigation/native-stack";
+import { useCallback, useState } from "react";
 
-const HomeLayout = () => {
+const HeaderRight = (props: NativeStackHeaderRightProps) => (
+  <Link href={"/CreateJob"} asChild>
+    <BorderlessButton>
+      <Icon name="plus" color={props.tintColor} />
+    </BorderlessButton>
+  </Link>
+);
+
+const Home = () => {
+  const [delay, setDelay] = useState(true);
+
+  useFocusEffect(
+    useCallback(() => {
+      setTimeout(() => setDelay(false), 200);
+    }, []),
+  );
+
   return (
     <Stack
       screenOptions={{
@@ -9,8 +29,22 @@ const HomeLayout = () => {
         animation: "slide_from_right",
         animationDuration: 250,
       }}
-    />
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Jobs",
+          headerRight: (props) => (delay ? null : <HeaderRight {...props} />),
+        }}
+      />
+      <Stack.Screen name="CreateJob" options={{ title: "New Job" }} />
+      <Stack.Screen name="[jobId]/edit" options={{ title: "Edit Job" }} />
+      <Stack.Screen
+        name="[jobId]/[paycycleId]/[shiftId]/index"
+        options={{ title: "Edit Shift" }}
+      />
+    </Stack>
   );
 };
 
-export default HomeLayout;
+export default Home;
